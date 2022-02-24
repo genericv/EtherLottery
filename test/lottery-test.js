22c4e4f6d70a970aec1a2791edaf501f52dbcaa7
genericv <viktor.bordyuzha@gmail.com>
2022-02-24 03:34:34 +0200

    test(lottery): update lottery ending tests

--- test/lottery-test.js
+++ test/lottery-test.js
@@ -140,70 +140,72 @@ describe("Lottery contract", function () {
     describe("Ending lottery", function () {
 
         it("Should correctly calculate winner's and beneficiary's respective rewards", async function () {
-            await hardhatLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther(ethTicketSupply)});
+            await hardhatToken.connect(addr1).buyLotteryTickets(hardhatLottery.address, ticketSupply);
             const [winnerReward, beneficiaryReward] = await hardhatLottery.getRewards();
             //The winner's reward should be 90% of the total reward.
-            expect(winnerReward).to.equal(ethers.utils.parseEther(Math.floor(+ethTicketSupply * 0.9) + ""));
+            expect(winnerReward).to.equal(ticketSupply * 0.9 * ticketPrice);
             //The winner's reward should be 10% of the total reward.
-            expect(beneficiaryReward).to.equal(ethers.utils.parseEther(Math.ceil(+ethTicketSupply * 0.1) + ""));
+            expect(beneficiaryReward).to.equal(ticketSupply * 0.1 * ticketPrice);
         });
 
         it("Should randomly select the winner among the players and send rewards to him and the beneficiary", async function () {
             const potentialWinners = [addr1.address, addr2.address];
-            const potentialWinnerBalances = [await provider.getBalance(addr1.address), await provider.getBalance(addr2.address)];
-            const initialBeneficiaryBalance = await provider.getBalance(beneficiary.address);
-            const halfOfTotalTicketAmount = Math.floor(ethTicketSupply / 2) + "";
+            const halfOfTotalTicketAmount = ticketSupply / 2;
             //Two players buy all of the tickets (each gets a half of the total amount).
-            await hardhatLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther(halfOfTotalTicketAmount)});
-            await hardhatLottery.connect(addr2).buyTickets({value: ethers.utils.parseEther(halfOfTotalTicketAmount)});
+            await hardhatToken.connect(addr1).buyLotteryTickets(hardhatLottery.address, halfOfTotalTicketAmount);
+            await hardhatToken.connect(addr2).buyLotteryTickets(hardhatLottery.address, halfOfTotalTicketAmount);
+            const potentialWinnerBalances = [await hardhatToken.balanceOf(addr1.address), await hardhatToken.balanceOf(addr1.address)];
+            const initialBeneficiaryBalance = await hardhatToken.balanceOf(beneficiary.address);
             await hardhatLottery.endLottery();
             const winnerAddress = await hardhatLottery.getWinner();
             //Winner should be one of the two players that bought all of the tickets.
             expect(winnerAddress).to.be.oneOf([addr1.address, addr2.address]);
 
+            const [winnerReward, beneficiaryReward] = await hardhatLottery.getRewards();
             const initialWinnerBalance = potentialWinnerBalances[potentialWinners.indexOf(winnerAddress)];
-            const finalWinnerBalance = await provider.getBalance(winnerAddress);
-            const finalBenificiaryBalance = await provider.getBalance(beneficiary.address);
-            const lotteryEthBalance =  await provider.getBalance(hardhatLottery.address);
-            // Winner's ethereum balance shoud be above his initial balance.
-            expect(finalWinnerBalance).to.be.above(initialWinnerBalance);
-            // Beneficiary's ethereum balance shoud be above his initial balance.
-            expect(finalBenificiaryBalance).to.be.above(initialBeneficiaryBalance);
-            // The contract balance should be equal to zero.
-            expect(lotteryEthBalance).to.equal(0);
+            const finalWinnerBalance = await hardhatToken.balanceOf(winnerAddress);
+            const finalBenificiaryBalance = await hardhatToken.balanceOf(beneficiary.address);
+            const lotteryBalance =  await hardhatToken.balanceOf(hardhatLottery.address);
+            // Winner's token balance shoud be increased by his reward.
+            expect(finalWinnerBalance).to.equal(initialWinnerBalance.add(winnerReward));
+            // Beneficiary's token balance shoud be increased by his reward.
+            expect(finalBenificiaryBalance).to.equal(initialBeneficiaryBalance.add(beneficiaryReward));
+            // The contract token balance should be equal to zero.
+            expect(lotteryBalance).to.equal(0);
         });
 
         it("Should fail if a player tries to end the lottery before all of the tickets have been sold", async function () {
-            const halfOfTotalTicketAmount = ethers.utils.parseEther(Math.floor(+ethTicketSupply / 2) + "");
-            await hardhatLottery.connect(addr1).buyTickets({value: halfOfTotalTicketAmount});
+            const halfOfTotalTicketAmount = ticketSupply / 2;
+            await hardhatToken.connect(addr1).buyLotteryTickets(hardhatLottery.address, halfOfTotalTicketAmount);
             // Only a half of the total ticket amount has been bought.
-            await expect(hardhatLottery.endLottery()).to.be.revertedWith("LotteryNotYetEnded");
+            await expect(hardhatLottery.endLottery()).to.be.revertedWith("The lottery has not ended yet.");
         });
 
         it("Should not fail if a player tries to end the lottery after the ticket buying period is over", async function () {
-            const halfOfTotalTicketAmount = ethers.utils.parseEther(Math.floor(+ethTicketSupply / 2) + "");
-            await hardhatLottery.connect(addr1).buyTickets({value: halfOfTotalTicketAmount});
+            const halfOfTotalTicketAmount = ticketSupply / 2;
+            await hardhatToken.connect(addr1).buyLotteryTickets(hardhatLottery.address, halfOfTotalTicketAmount);
             //Fast forward to the end of the ticket buying period.
             await network.provider.send("evm_increaseTime", [duration + 1]);
             // Only a half of the total ticket amount has been bought,
             // but the ticket buying period has ended.
-            await expect(hardhatLottery.endLottery()).to.not.be.reverted;
+            await hardhatLottery.endLottery()
         });
 
         it("Should fail if a player tries to end the lottery that has already ended", async function () {
-            const halfOfTotalTicketAmount = ethers.utils.parseEther(Math.floor(ethTicketSupply / 2) + "");
-            await hardhatLottery.connect(addr1).buyTickets({value: halfOfTotalTicketAmount});
-            await hardhatLottery.connect(addr2).buyTickets({value: halfOfTotalTicketAmount});
+            const halfOfTotalTicketAmount = ticketSupply / 2;
+            //Two players buy all of the tickets (each gets a half of the total amount).
+            await hardhatToken.connect(addr1).buyLotteryTickets(hardhatLottery.address, halfOfTotalTicketAmount);
+            await hardhatToken.connect(addr2).buyLotteryTickets(hardhatLottery.address, halfOfTotalTicketAmount);
             // The end of the lottery.
             await hardhatLottery.endLottery();
             // The lottery can not be ended twice.
-            await expect(hardhatLottery.endLottery()).to.be.revertedWith("LotteryEndAlreadyCalled");
+            await expect(hardhatLottery.endLottery()).to.be.revertedWith("The lottery ending has already been requested.");
         });
 
         it("Should emit an event containing the winner's address and reward", async function () {
-            await hardhatLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther(ethTicketSupply)});
+            await hardhatToken.connect(addr1).buyLotteryTickets(hardhatLottery.address, ticketSupply);
             await expect(hardhatLottery.endLottery()).to.emit(hardhatLottery, 'LotteryEnded')
-            .withArgs(addr1.address, ethers.utils.parseEther(Math.floor(+ethTicketSupply * 0.9) + ""));
+            .withArgs(addr1.address, ticketSupply * 0.9 * ticketPrice);
         });
     });
 
